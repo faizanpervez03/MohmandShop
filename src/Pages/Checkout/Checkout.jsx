2017e80263865e8bee3aa0e4053bf3682be0af0a
faizanpervez03 <faizanpervez005@gmail.com>
2025-02-16 14:11:46 +0500

    form updated

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,170 +1,287 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 
 const Checkout = () => {
-    return (
-        <>
-            <div class="hero mt-5">
-                <div class="container">
-                    <div class="row justify-content-between">
-                        <div class="col-lg-5">
-                            <div class="intro-excerpt">
-                                <h1 className='text-3xl'>Checkout</h1>
-                            </div>
-                        </div>
-                        <div class="col-lg-7">
+  const [orderData, setOrderData] = useState({
+    user: {
+      userId: 1,
+      firstName: " ",
+      lastName: "",
+      address: "",
+      city: "",
+      phone: "",
+    },
+    products: [
+     {
+        productId: 1,
+        quantity: 4
+     },
+     {
+        productId: 2,
+        quantity: 4  
 
-                        </div>
-                    </div>
-                </div>
+     }
+    ]
+  });
+
+
+  const handInputChange = (e) => {
+    const {name, value} = e.target
+    setOrderData((prevData) => ({
+      ...prevData,
+      user: {
+        ...prevData.user,
+        [name]: value
+      },
+    }) )
+  }
+ console.log(orderData);
+ 
+  const handlePlaceOrder = () => {
+    console.log("Order Data Submitted:", orderData); 
+    
+  };
+
+
+
+  // ---------------------------------------------------------
+  const { cart } = useSelector((state) => state.cartSlice);
+
+  const getTotal = () => {
+    return cart.reduce((total, item) => total + item.price * item.quantity, 300);
+  };
+  // ----------------------------------------------------------
+  return (
+    <>
+      <div class="hero mt-5">
+        <div class="container">
+          <div class="row justify-content-between">
+            <div class="col-lg-5">
+              <div class="intro-excerpt">
+                <h1 className="text-3xl">Checkout</h1>
+              </div>
             </div>
-            {/* <!-- End Hero Section --> */}
+            <div class="col-lg-7"></div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- End Hero Section --> */}
 
-            <div class="untree_co-section">
-                <div class="container">
-                    {/* <div class="row mb-5">
-                        <div class="col-md-12">
-                            <div class="border p-4 rounded" role="alert">
-                                Returning customer? <a href="#">Click here</a> to login
-                            </div>
-                        </div>
-                    </div> */}
-                    <div class="row">
-                        <div class="col-md-6 mb-5 mb-md-0">
-                            <h2 class="h3 mb-3 text-black"  >Billing Details</h2>
-                            <div class="p-3 p-lg-5 border bg-white">
-                                
-                                <div class="form-group row">
-                                    <div class="col-md-6">
-                                        <label for="c_fname" class="text-black">First Name <span class="text-danger">*</span></label>
-                                        <input type="text" required class="form-control" id="c_fname" name="c_fname" />
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="c_lname" class="text-black">Last Name <span class="text-danger">*</span></label>
-                                        <input type="text" required class="form-control" id="c_lname" name="c_lname" />
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <div class="col-md-12">
-                                        <label for="c_address" class="text-black">Address <span class="text-danger">*</span></label>
-                                        <input type="text" required className="form-control" id="c_address" name="c_address" placeholder="Street address" />
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <div class="col-md-6">
-                                        <label for="c_state_country" class="text-black">State / City <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" id="c_state_country" name="c_state_country" />
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="c_postal_zip" class="text-black">Postal / Zip <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" id="c_postal_zip" name="c_postal_zip" />
-                                    </div>
-                                </div>
-
-                                <div class="form-group row mb-5">
-                                    <div class="col-md-6">
-                                        <label for="c_email_address" class="text-black">Email Address <span class="text-danger">*</span></label>
-                                        <input type="text"  class="form-control" id="c_email_address" name="c_email_address" />
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="c_phone" class="text-black">Phone <span class="text-danger">*</span></label>
-                                        <input type="text" required class="form-control" id="c_phone" name="c_phone" placeholder="Phone Number" />
-                                    </div>
-                                </div>
-
-                                
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-
-                         
-                            <div class="row mb-5">
-                                <div class="col-md-12">
-                                    <h2 class="h3 mb-3 text-black">Your Order</h2>
-                                    <div class="p-3 p-lg-5 border bg-white">
-                                        <table class="table site-block-order-table mb-5">
-                                            <thead>
-                                                <th>Product</th>
-                                                <th>Total</th>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>Top Up Chair Nordic <strong class="mx-2">x</strong> 1</td>
-                                                    <td>Rs.2000</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>New chair <strong class="mx-2">x</strong>   1</td>
-                                                    <td>Rs.3400</td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="text-black font-weight-bold"><strong>Cart Subtotal</strong></td>
-                                                    <td class="text-black">Rs.2000</td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="text-black font-weight-bold"><strong>Order Total</strong></td>
-                                                    <td class="text-black font-weight-bold"><strong>Rs.3400</strong></td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-
-                                        <div class="border p-3 mb-3">
-                                            <h3 class="h6 mb-0">
-                                                <Link class="d-block" data-bs-toggle="collapse" href="#collapsebank" role="button" aria-expanded="false" aria-controls="collapsebank">Direct Bank Transfer</Link>
-                                            </h3>
-
-                                            <div class="collapse" id="collapsebank">
-                                                <div class="py-2">
-                                                    <p class="mb-0">Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="border p-3 mb-3">
-                                            <h3 class="h6 mb-0">
-                                                <Link class="d-block" data-bs-toggle="collapse" href="#collapsecheque" role="button" aria-expanded="false" aria-controls="collapsecheque">Cheque Payment</Link>
-                                            </h3>
-
-
-                                            <div class="collapse" id="collapsecheque">
-                                                <div class="py-2">
-                                                    <p class="mb-0">Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="border p-3 mb-5">
-                                            <h3 class="h6 mb-0">
-                                                <Link class="d-block" data-bs-toggle="collapse" href="#collapsepaypal" role="button" aria-expanded="false" aria-controls="collapsepaypal">Paypal</Link>
-
-
-                                            </h3>
-
-                                            <div class="collapse" id="collapsepaypal">
-                                                <div class="py-2">
-                                                    <p class="mb-0">Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <Link to="/Thankyou">
-                                            <button class="btn btn-black btn-lg py-3 btn-block" onclick="window.location='thankyou.html'">Place Order</button>
-                                            </Link>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    {/* <!-- </form> --> */}
+      <div class="untree_co-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 mb-5 mb-md-0">
+              <h2 class="h3 mb-3 text-black">Billing Details</h2>
+              <div class="p-3 p-lg-5 border bg-white">
+                <div class="form-group row">
+                  <div class="col-md-6">
+                    <label htmlFor="c_fname" class="text-black">
+                      First Name <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      required
+                      class="form-control"
+                      id="c_fname"
+                      name="c_fname"
+                      value={orderData.user.firstName}
+                      onChange={handInputChange}
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <label htmlFor="c_lname" class="text-black">
+                      Last Name <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      required
+                      class="form-control"
+                      id="c_lname"
+                      name="c_lname"
+                      value={orderData.user.lastName}
+                      onChange={handInputChange}
+                    />
+                  </div>
                 </div>
-            </div>
-        </>
-    )
-}
 
-export default Checkout
+                <div class="form-group row">
+                  <div class="col-md-12">
+                    <label htmlFor="c_address" class="text-black">
+                      Address <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      required
+                      className="form-control"
+                      id="c_address"
+                      name="c_address"
+                      placeholder="Street address"
+                      value={orderData.user.address}
+                      onChange={handInputChange}
+                    />
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <div class="col-md-6">
+                    <label htmlFor="c_state_country" class="text-black">
+                      State / City <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="c_state_country"
+                      name="c_state_country"
+                      value={orderData.user.city}
+                      onChange={handInputChange}
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <label htmlFor="c_postal_zip" class="text-black">
+                      Postal / Zip <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="c_postal_zip"
+                      name="c_postal_zip"
+                    />
+                  </div>
+                </div>
+
+                <div class="form-group row mb-5">
+                  <div class="col-md-6">
+                    <label htmlFor="c_email_address" class="text-black">
+                      Email Address <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="c_email_address"
+                      name="c_email_address"
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <label htmlFor="c_phone" class="text-black">
+                      Phone <span class="text-danger">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      required
+                      class="form-control"
+                      id="c_phone"
+                      name="c_phone"
+                      placeholder="Phone Number"
+                      value={orderData.user.phone}
+                      onChange={handInputChange}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="row mb-5">
+                <div class="col-md-12">
+                  <h2 class="h3 mb-3 text-black">Your Order</h2>
+                  <div class="p-3 p-lg-5 border bg-white">
+                    <table class="table site-block-order-table mb-5">
+                      <thead>
+                        <th>Products</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                      </thead>
+                      <tbody>
+                        {cart.map((item) => (
+                          <tr key={item.id}>
+                            <td>
+                              <img src={item.imageUrl}
+                               alt=""
+                               className="w-16 h-16 object-cover"
+                               />
+                            </td>
+                            <td>
+                              {item.title}
+                            </td>
+                            <td >
+                              Rs. {item.price}
+                            </td>
+                           <td className="text-center">
+                            {item.quantity}
+                           </td>
+                            <td >
+                              Rs. {item.price * item.quantity}
+                            </td>
+                            
+                          </tr>
+                        ))}
+
+                       
+
+                        <tr style={{borderTop:"2px solid black"}}>
+                    
+
+                          <td class="text-black font-weight-bold" >
+                            <strong>Order Total</strong>
+                          </td>
+                          <td class="text-black font-weight-bold">
+                            <strong>Rs.{getTotal()}</strong>
+                          </td>
+                        </tr>
+
+                         <tr >
+                        <td className=" w-32">
+                            <strong>COD Charges</strong>
+                        </td>
+
+                        <td>
+                            <strong>Rs. 300</strong>
+                            
+                        </td>
+                        </tr>
+
+                      </tbody>
+                    </table>
+
+                    <div class="border p-3 mb-3">
+                      <h3 class="h6 mb-0">
+                        <Link
+                          class="d-block"
+                          data-bs-toggle="collapse"
+                          href="#collapsebank"
+                          aria-expanded="false"
+                          aria-controls="collapsebank"
+                        >
+                          COD
+                        </Link>
+                      </h3>
+                    </div>
+
+                
+
+                    <div class="form-group">
+                      <Link to="/Thankyou">
+                        <button
+                          class="btn btn-black btn-lg py-3 btn-block"
+                          onClick={handlePlaceOrder}
+                        >
+                          Place Order
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <!-- </form> --> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
